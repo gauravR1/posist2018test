73ec8e856d3d13475fa4a6c3b2a954748f6d6c38
Gaurav Rai <gauravraiofficial@gmail.com>
2018-08-29 17:35:28 +0530

    Update node.c

--- node.c
+++ node.c
@@ -13,8 +13,6 @@ struct node
 	char referenceNodeId[50];
 	struct node *childReferenceNode[2];
 	struct node *genesisReferenceNodeID;
-
-	// since I do not know how to create hash value i am just using a string value to denote hashValue 
 	char hashValue[50];	
 	
 
